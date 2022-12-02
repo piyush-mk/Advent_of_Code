cb007a3d99cc6fd2bec5acb023cd69a792ae3b23
Piyush Mohapatra <piyush.ofcl@gmail.com>
2022-12-02 20:10:05 +0530

    Day1 Solution

---
+++ Day1/1.py
@@ -0,0 +1,33 @@
+import heapq
+maxValue = 0
+curValue = 0 
+with open("input.txt", "r") as reader:
+    for line in reader.readlines():
+        line = line.strip()
+        if len(line) == 0:
+            maxValue = max(maxValue,curValue)
+            curValue = 0
+        else:
+            curValue += int(line)
+ 
+print(f"The max value was: {maxValue}")
+ 
+h = []
+curValue = 0
+with open("input.txt", "r") as reader:
+ 
+    for line in reader.readlines():
+        line = line.strip()
+        if len(line) == 0:
+            heapq.heappush(h, curValue)
+ 
+            if len(h) > 3:
+                heapq.heappop(h)
+            
+            curValue = 0
+        else:
+            curValue += int(line)
+ 
+total = sum(h)
+ 
+print(f"The total is: {total}")
