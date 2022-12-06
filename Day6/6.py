f9dfdb6e8740538ed78972040580beff5b784f06
Piyush Mohapatra <piyush.ofcl@gmail.com>
2022-12-06 12:21:37 +0530

    Comments on Day6

--- Day6/6.py
+++ Day6/6.py
@@ -1,17 +1,12 @@
 import os
 filename = "input6.txt"
-here = os.path.dirname(os.path.abspath(__file__))
-filepath = os.path.join(here, filename)
-
-with open(filepath, "r") as f:
-    content = f.read()
-
+with open(filename, "r") as f:
+    content = f.read()  
 def func(distinct):
     global content
-    for i in range(distinct-1, len(content)):
-        templst = [content[i-x] for x in range(distinct)]
-        if templst == list(dict.fromkeys(templst)):
-            return str(i+1)
-
-print("Part 1:", func(4))
-print("Part 2:", func(14))
+    for i in range(distinct-1, len(content)):               
+        templst = [content[i-x] for x in range(distinct)]   # create a list of the last distinct characters
+        if templst == list(dict.fromkeys(templst)):        # if the list contains no duplicates
+            return str(i+1)                               # return the index of the first character in the list
+print("Part 1:", func(4))   # print the index of the first character in the list of the last 4 characters
+print("Part 2:", func(14))  # print the index of the first character in the list of the last 14 characters
